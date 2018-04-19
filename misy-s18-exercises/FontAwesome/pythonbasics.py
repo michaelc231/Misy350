f171cd9803b00d07eefef7fddf2ef6ae5e7d6bd0
Michael Connelly <mikecon@udel.edu>
2018-04-19 10:24:44 -0400

    in ckass

---
+++ misy-s18-exercises/FontAwesome/pythonbasics.py
@@ -0,0 +1,10 @@
+#this is my first python code
+greeting = "hello world!"
+name = "My name is Mike"
+
+print greeting   #this prints out a string
+print len(greeting)
+print greeting.upper()
+print greeting[4]
+print greeting.upper() + " " + name
+print "%s %s" % (greeting, name)
